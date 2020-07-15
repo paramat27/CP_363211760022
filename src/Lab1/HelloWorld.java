7e8cbfddad8fb441c11de7cad20bee62a69af5a7
Paramat <poramat.p@rmutsvmail.com>
2020-07-15 14:54:06 +0700

    add more code

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -5,6 +5,11 @@ public class HelloWorld
     public static void main(String[] args) {
 
         System.out.println("Hello World!!!");
+        System.out.println("Paramat Pantawip");
+        System.out.println("363211760022");
+        System.out.println("คณะเทคโลยีการจัดการ");
+        System.out.println("การจัดการเทคโนโลยีสารสนเทศ");
+
 
 
     }
